c86257f9f0a677cfad61a699440fa60a7eeaf9a0
sahiti07 <adhikarla.sahiti@gmail.com>
2019-04-06 14:02:47 +0530

    NEW ELEMENTS

---
+++ 50.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+p1 = Person("Sahiti", 13)
+
+print(p1.name)
+print(p1.age)
+
+
+p2 = Person("Asha", 36)
+
+print(p2.name)
+print(p2.age)
---
+++ 51.py
@@ -0,0 +1,7 @@
+try:
+    print("Hello World")
+
+except:
+    print("Something went wrong")
+else:
+    print("Nothing went wrong")
